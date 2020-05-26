9c880a7de866b4e27949fc37e7285840b67a5a2e
KuznecovaDarya <darya.kuznetsoffa@gmail.com>
2020-05-26 19:37:54 +0300

    as4

---
+++ ConsoleApp33/ConsoleApp33/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using static System.Convert;
+using static System.Console;
+using studentlib;
+
+namespace ConsoleApp32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Введи сегодняшнюю дату: ");
+            WriteLine("День: ");
+            int day = ToInt32(ReadLine());
+            WriteLine("Месяц: ");
+            int month = ToInt32(ReadLine());
+            WriteLine("Год: ");
+            int year = ToInt32(ReadLine());
+
+            Proect a = new Proect();
+            int n = 1;
+            while (n > 0)
+            {
+                WriteLine("1 - Показать Информацию о студентах.\n2 - Добавить студента.\n3 - Удалить студента.\n4 - Изменить студента.\n5 - Поиск студентов.\n6 - Выход.");
+                n = ToInt32(ReadLine());
+                if (n == 1)
+                {
+                    WriteLine("1 - Вся информация о студентах.\n2 - Список студентов с инициалами.\n3 - Список студентов старше 18.\n4 - Список студентов младше 18.");
+                    int m = ToInt32(ReadLine());
+                    if (m == 1)
+                    {
+                        a.show();
+                    }
+                    else if (m == 2)
+                    {
+                        a.inic();
+                    }
+                    else if (m == 3)
+                    {
+                        a.Vozvr(m, day, month, year);
+                    }
+                    else if (m == 4)
+                    {
+                        a.Vozvr(m, day, month, year);
+                    }
+                    WriteLine("Введи следующее действие: ");
+                }
+                else if (n == 2)
+                {
+                    WriteLine("ID: "); string id = ReadLine();
+                    WriteLine("ФИО: "); string fio = ReadLine();
+                    WriteLine("Группа: "); string group = ReadLine();
+                    WriteLine("Дата рождения: "); string data = ReadLine();
+
+                    a.add(id, fio, group, data);
+                    WriteLine("Введи следующее действие: ");
+                }
+                else if (n == 4)
+                {
+                    WriteLine("Введи ID и измененные данные: ");
+                    WriteLine("ID: "); string id = ReadLine();
+                    WriteLine("ФИО: "); string fio = ReadLine();
+                    WriteLine("Группа: "); string group = ReadLine();
+                    WriteLine("Дата: "); string data = ReadLine();
+                    a.izmenie(id, fio, group, data);
+                    WriteLine("Введи следующее действие: ");
+                }
+                else if (n == 3)
+                {
+                    WriteLine("Введи ID: ");
+                    string id = ReadLine();
+                    a.del(id);
+                    WriteLine("Введи следующее действие: ");
+                }
+                else if (n == 5)
+                {
+                    WriteLine("1 - поиск по ID.\n2 - поиск по фамилии\n");
+                    int m = ToInt32(ReadLine());
+                    if (m == 1)
+                    {
+                        WriteLine("Введи ID: ");
+                        string id = ReadLine();
+                        a.viv(id);
+                    }
+                    else if (m == 2)
+                    {
+                        WriteLine("Введи фамилию: ");
+                        string famil = ReadLine();
+                        a.famp(famil);
+                    }
+                    WriteLine("Введи следующее действие: ");
+                }
+                else if (n == 6)
+                {
+                    Environment.Exit(0);
+                }
+            }
+        }
+    }
+}
---
+++ studentlib/studentlib/Proect.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using static System.Convert;
+using static System.Console;
+using System.Collections.Generic;
+
+namespace studentlib
+{
+    public class Proect
+    {
+        public class student
+        {
+            public string id;
+            public string fio;
+            public string group;
+            public string data;
+        }
+        public List<student> list = new List<student>();
+        public void add(string id, string fio, string group, string data)
+        {
+            list.Add(new student() { id = id, fio = fio, group = group, data = data });
+        }
+        public void del(string id)
+        {
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i].id == id) list.RemoveAt(i);
+            }
+
+        }
+        public void izmenie(string id, string fio, string group, string data)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i].id == id)
+                {
+                    list[i].fio = fio;
+                    list[i].group = group;
+                    list[i].data = data;
+                }
+            }
+        }
+        public void viv(string id)
+        {
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i].id == id)
+                {
+                    Console.WriteLine(list[i].id + " " + list[i].fio + " " + list[i].group + " " + list[i].data);
+                }
+            }
+
+        }
+        public void years(string id, int day1, int month1, int year1)
+        {
+            int t1;
+            int t2;
+            int vozr;
+            string day = "";
+            string month = "";
+            string year = "";
+            string dat = "";
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i].id == id)
+                {
+                    dat = list[i].data + "";
+                }
+            }
+            t1 = dat.IndexOf(".");
+            t2 = dat.LastIndexOf(".");
+            for (int i = 0; i < t1; i++)
+            {
+                day = day + dat[i];
+            }
+            for (int i = t1 + 1; i < t2; i++)
+            {
+                month = month + dat[i];
+            }
+            for (int i = t2 + 1; i < dat.Length; i++)
+            {
+                year = year + dat[i];
+            }
+            Console.WriteLine("Дата рождения" + day + "." + month + "." + year);
+            vozr = year1 - ToInt32(year);
+            if (ToInt32(month) > month1) vozr = vozr - 1;
+            else if (ToInt32(month) == month1)
+                if (ToInt32(day) < day1) vozr = vozr - 1;
+            Console.WriteLine("\nВозраст = " + vozr);
+        }
+        public void show()
+        {
+            foreach (var i in list)
+            {
+                Console.WriteLine(i.id + " " + i.fio + " " + i.group + " " + i.data);
+            }
+
+        }
+        public void inic()
+        {
+            string prom = "";
+            for (int i = 0; i < list.Count; i++)
+            {
+                string rez = "";
+                prom = list[i].fio;
+                string[] words = prom.Split(' ');
+                rez = rez + words[0] + " " + words[1][0] + "." + words[2][0] + ".";
+                Console.WriteLine(rez);
+            }
+        }
+        public void famp(string fam)
+        {
+            string prom = "";
+            for (int i = 0; i < list.Count; i++)
+            {
+                prom = list[i].fio;
+                string[] words = prom.Split(' ');
+                if (fam == words[0])
+                {
+                    Console.WriteLine(list[i].id + " " + list[i].fio + " " + list[i].group + " " + list[i].data);
+                }
+            }
+        }
+        public void Vozvr(int param, int day1, int month1, int year1)
+        {
+            for (int g = 0; g < list.Count; g++)
+            {
+                int t1;
+                int t2;
+                int vozr;
+                string day = "";
+                string month = "";
+                string year = "";
+                string dat;
+                string id;
+
+                id = list[g].id;
+                dat = list[g].data + "";
+
+                t1 = dat.IndexOf(".");
+                t2 = dat.LastIndexOf(".");
+                for (int i = 0; i < t1; i++)
+                {
+                    day = day + dat[i];
+                }
+                for (int i = t1 + 1; i < t2; i++)
+                {
+                    month = month + dat[i];
+                }
+                for (int i = t2 + 1; i < dat.Length; i++)
+                {
+                    year = year + dat[i];
+                }
+                vozr = year1 - ToInt32(year);
+                if (ToInt32(month) > month1) vozr = vozr - 1;
+                else if (ToInt32(month) == month1)
+                    if (ToInt32(day) < day1) vozr = vozr - 1;
+                if (param == 4) if (vozr < 18) Console.WriteLine(list[g].fio + " ");
+                if (param == 3) if (vozr > 18) Console.WriteLine(list[g].fio + " ");
+            }
+        }
+    }
+}
